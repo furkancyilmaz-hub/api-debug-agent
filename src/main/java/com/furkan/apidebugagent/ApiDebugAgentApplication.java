c6cb147419a3be618cdb1f26ff2371dd5ecb69d3
furkancyilmaz-hub <cevfurkanyilmaz@gmail.com>
2026-08-05 00:16:40 +0300

    Initial commit

---
+++ src/main/java/com/furkan/apidebugagent/ApiDebugAgentApplication.java
@@ -0,0 +1,13 @@
+package com.furkan.apidebugagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDebugAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDebugAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/furkan/apidebugagent/ApiDebugAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.furkan.apidebugagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDebugAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
